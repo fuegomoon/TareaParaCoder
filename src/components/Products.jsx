2263de74a23fc5c54999a9d043384044d7c79faa
Michelle <fuegomoon@yahoo.com>
2025-02-05 19:22:59 -0300

    now images actually fixed

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import cheescake from './images/cheescake.jpeg';
-import pistachio from './images/pistachio.jpeg';
-import tiramisu from './images/tiramisu.jpeg';
-import chiffon from './images/chiffon.jpeg';
-import chocolate from './images/chocolate.jpeg';
-import rosewater from './images/raspberry-rosewater.jpeg';  
+import './Products.css';
+import cheescake from '../images/cheescake.jpeg';
+import pistachio from '../images/pistachio.jpeg';
+import tiramisu from '../images/tiramisu.jpeg';
+import chiffon from '../images/chiffon.jpeg';
+import chocolate from '../images/chocolate-cake.jpeg';
+import rosewater from '../images/raspberry-rosewater.jpeg';  
 
 const products = [
   { id: 1, name: 'Chocolate Dream', price: 8.900, image: chocolate },
